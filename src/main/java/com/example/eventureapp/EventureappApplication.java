21762d19f5a54df69d1adbc1d69e0a11129b84fe
Sarmad <sarmad04@outlook.com>
2025-06-09 20:23:55 +0200

    first commit

---
+++ src/main/java/com/example/eventureapp/EventureappApplication.java
@@ -0,0 +1,13 @@
+package com.example.eventureapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventureappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventureappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/eventureapp/EventureappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eventureapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventureappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
