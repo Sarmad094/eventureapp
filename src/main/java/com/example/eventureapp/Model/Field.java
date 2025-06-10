cedb66bfedfd93c2478bbf46464e82d0a7d14ab6
Zubayraev <zubayraevzaurbek@gmail.com>
2025-06-10 15:57:27 +0200

    field2

--- src/main/java/com/example/eventureapp/Model/Field.java
+++ src/main/java/com/example/eventureapp/Model/Field.java
@@ -3,6 +3,7 @@ package com.example.eventureapp.Model;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import java.util.List;
 
 @Entity
 @Getter
@@ -11,19 +12,33 @@ public class Field {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "field_id")
     private Long fieldId;
 
-    @Column(nullable = false)
-    private String field;
+    @Column(name = "field_name", nullable = false)
+    private String fieldName;
 
     @Column(name = "f_description")
     private String fDescription;
 
+    // One-to-Many relationship with Event
+    @OneToMany(mappedBy = "field", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Event> events;
+
     public Field() {
     }
 
-    public Field(String field, String fDescription) {
-        this.field = field;
+    public Field(String fieldName, String fDescription) {
+        this.fieldName = fieldName;
         this.fDescription = fDescription;
+    }
+
+    @Override
+    public String toString() {
+        return "Field{" +
+                "fieldId=" + fieldId +
+                ", fieldName='" + fieldName + '\'' +
+                ", fDescription='" + fDescription + '\'' +
+                '}';
     }
 }
